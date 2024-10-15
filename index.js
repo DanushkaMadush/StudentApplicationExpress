c877fe3fb313e2b0ed891618056d738d48de73bf
danushkacodeolima <danushka@codeolima.com>
2024-10-15 22:26:34 +0530

    routes added

--- index.js
+++ index.js
@@ -1,68 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Product = require("./models/student");
-const Student = require("./models/student");
+const Student = require("./models/student.js");
+const studentRoute = require("./routes/route.js")
+
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
-app.post("/api/students", async (req, res) => {
-  try {
-    const student = await Student.create(req.body);
-    res.status(201).json(student);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/students", async (req, res) => {
-  try {
-    const students = await Student.find({});
-    res.status(200).json(students);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/students/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const student = await Student.findById(id);
-    res.status(200).json(student);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.put("/api/students/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const student = await Student.findByIdAndUpdate(id, req.body);
-
-    if (!student) {
-      return res.status(404).json({ message: "Student not found" });
-    }
-    const updatedStudent = await Student.findById(id);
-    res.status(200).json(updatedStudent);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.delete("/api/students/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const student = await Student.findByIdAndDelete(id);
-
-    if (!student) {
-      return res.status(404).json({ message: "Student not found" });
-    }
-
-    res.status(200).json({ message: "Student deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+//routes
+app.use("/api/students" , studentRoute);
 
 mongoose
   .connect("mongodb://localhost:27017/StudentApplication")
