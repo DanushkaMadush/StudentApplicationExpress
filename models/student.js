2c4e93a4e8af64e1624df7e6ac150b2462983ea3
danushkacodeolima <danushka@codeolima.com>
2024-10-15 20:39:32 +0530

    model created

---
+++ models/student.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+
+const StudentSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+    },
+
+    studentEmail: {
+      type: String,
+      required: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+    },
+
+    address: {
+      type: String,
+      required: false,
+    },
+
+    country: {
+      type: String,
+      required: true,
+    },
+
+    institute: {
+      type: String,
+      required: true,
+    },
+
+    intake: {
+      type: String,
+      required: true,
+    },
+
+    courseTitle: {
+      type: String,
+      required: true,
+    },
+
+    licenseStatus: {
+      type: String,
+      required: false,
+      default: "Inactive",
+    },
+
+    approvalStatus: {
+      type: String,
+      required: false,
+      default: "Pending",
+    },
+
+    expiryDate: {
+      type: String,
+      required: false,
+      default: "N/A",
+    },
+  },
+  { timestamps: true }
+);
+
+const Student = mongoose.model("Students", StudentSchema);
+
+module.exports = Student;
